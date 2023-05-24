fc7357edb700084a6733e86c69ac33ba38bd2261
Prismly <prismly55@gmail.com>
2023-05-24 18:00:54 -0400

    AgentInfo typo fix, Changing Focused Agent
    - Small typo fix ('accommplishment' to 'accomplishment') in AgentInfo
    - Implemented cycling through selected agents in the AgentPanel, no visual feedback yet
    - Laid out motive bar headers in UI Framework scene

--- Assets/Scripts/AgentPanel.cs
+++ Assets/Scripts/AgentPanel.cs
@@ -1,13 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class AgentPanel : Panel
 {
     [Header(" --- Agent Info Panel ---")]
     private List<Agent> selectedAgents;
-    private int focusedAgentIdx;
-   
+    private int focusedAgentIdx = 0;
+
+    // TODO: These UI objects display the information relating to the currently focused agent.
+    // However, since this is intended to be modifiable, in the future these should be instantiated on startup.
+
+
+    public void ChangeFocusedAgent(int indexDelta)
+    {
+        // Obtain the actual number of indices moved, after full loop-arounds are taken into account.
+        // I'm avoiding using a negative number in the modulo operation because implementations of that math depend on the programming language.
+        int clampedDelta = Mathf.Abs(indexDelta) % selectedAgents.Count;
+        focusedAgentIdx += clampedDelta;
+        // Because we clamped indexDelta, resolving cases where the index is out of bounds is just addition/subtraction.
+        if (focusedAgentIdx < 0)
+            focusedAgentIdx += selectedAgents.Count;
+        if (focusedAgentIdx >= selectedAgents.Count)
+            focusedAgentIdx -= selectedAgents.Count;
+
+        DisplayAgentInfo();
+    }
+
+    public void DisplayAgentInfo()
+    {
+
+    }
+
     /**
      * Extends the generic implementation of Panel.Show() to include setting a list of selected Agents.
      * @param selectedAgents is a list of Agent objects that represents all Agents currently viewable in the Agent Info Panel.
@@ -17,6 +42,7 @@ public class AgentPanel : Panel
         base.Show();
 
         this.selectedAgents = selectedAgents;
+        focusedAgentIdx = 0;
     }
 
     /**
@@ -37,5 +63,6 @@ public class AgentPanel : Panel
         base.Hide();
 
         selectedAgents.Clear();
+        focusedAgentIdx = 0;
     }
 }
