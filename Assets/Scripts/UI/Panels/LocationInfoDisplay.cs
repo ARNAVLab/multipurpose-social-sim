b49e28c67ea35f898f290125a6e5197e1c6bb4ed
Prismly <prismly55@gmail.com>
2023-07-03 16:44:50 -0400

    Reorganized File Structure, Production Scene
    - There is now a 'Production' scene, intended to be touched only to integrate completed features.
    - Cleaned up and rearranged Scenes, Prefabs, and Scripts folders

---
+++ Assets/Scripts/UI/Panels/LocationInfoDisplay.cs
@@ -0,0 +1,32 @@
+using Anthology.SimulationManager;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LocationInfoDisplay : MonoBehaviour
+{
+    private Location displayedLocation;
+
+    [SerializeField] private TextMeshProUGUI locationName;
+    [SerializeField] private Transform tagCollection;
+    [SerializeField] private GameObject tagPref;
+
+    public void DisplayLocationInfo(float xCoord, float yCoord)
+    {
+        System.Numerics.Vector2 locPos = new System.Numerics.Vector2(xCoord, yCoord);
+        Location prospLoc;
+        if (!SimManager.Locations.TryGetValue(locPos, out prospLoc))
+            return;
+
+        locationName.text = prospLoc.Name.FirstCharacterToUpper();
+
+        foreach (string tag in prospLoc.Tags)
+        {
+            GameObject tagDisp = Instantiate(tagPref);
+            tagDisp.GetComponent<TextMeshProUGUI>().text = tag;
+            tagDisp.transform.SetParent(tagCollection);
+        }
+    }
+}
