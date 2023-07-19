f246498d5cdf735bc97e59ffba4254192ded5e48
Prismly <prismly55@gmail.com>
2023-07-19 18:21:01 -0400

    Relationships Panel now displays actor relationships
    - To achieve this, I had to expose Relationships to the frontend via the NPC class.

--- Assets/Scripts/UI/Panels/RelationshipDisplay.cs
+++ Assets/Scripts/UI/Panels/RelationshipDisplay.cs
@@ -9,10 +9,12 @@ public class RelationshipDisplay : MonoBehaviour
     [SerializeField] private GameObject relationshipEntryPref;
     private Panel relationPanel;
     private List<GameObject> relationshipList = new List<GameObject>();
+    private Transform relationshipEntryContainer;
 
     private void Start()
     {
         relationPanel = GetComponent<Panel>();
+        relationshipEntryContainer = GetComponent<Panel>().content.transform;
         //WorldManager.simUpdated.AddListener(SimUpdateListener);
     }
 
@@ -26,19 +28,27 @@ public class RelationshipDisplay : MonoBehaviour
 
     public void DisplayActorRelations(NPC npc)
     {
+        foreach (GameObject relationshipObj in relationshipList)
+        {
+            Destroy(relationshipObj);
+        }
+        relationshipList.Clear();
         foreach (Relationship r in npc.Relationships)
         {
-            
+            AddRelationship(r.Type, r.With);
         }
     }
 
-    public void AddRelationship(string relateType, int relateeID)
+    public void AddRelationship(string relateType, string relateeName)
     {
         GameObject newEntryObj = Instantiate(relationshipEntryPref);
         RelationshipEntry newEntry = newEntryObj.GetComponent<RelationshipEntry>();
         newEntry.SetRelationshipType(relateType);
-        newEntry.SetRelateeName(WorldManager.GetAgent(relateeID).name);
-        newEntry.transform.SetParent(GetComponent<Panel>().content.transform);
-        newEntry.transform.SetAsLastSibling();
+        newEntry.SetRelateeName(relateeName);
+        
+        newEntryObj.transform.SetParent(relationshipEntryContainer);
+        //newEntry.transform.SetAsLastSibling();
+        newEntryObj.transform.localScale = Vector3.one;
+        relationshipList.Add(newEntryObj);
     }
 }
